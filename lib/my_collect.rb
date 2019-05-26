dd696d42d59a3bbef62ee941d1062cc7e228515b
Federica Macchi <federica.macchi3@gmail.com>
2019-05-26 10:56:04 +0100

    Done

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
 
-
+def my_collect(collection)
+    counter = 1
+    index = 0
+    new_collection = []
+    while counter <= collection.length
+        my_item = collection[index]
+        new_collection << yield(my_item)
+        counter += 1
+        index += 1
+    end
+    return new_collection
+end
